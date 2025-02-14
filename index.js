6d99ede48583719577106c0523a2ce104507c952
heeandika <165105890+heeandika@users.noreply.github.com>
2025-02-14 22:27:41 +0900

    test 2

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/siswa', siswaRouter);
+app.use('/', siswaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
